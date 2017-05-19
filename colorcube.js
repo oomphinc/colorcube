f34611cfb63f8183ba8a5971421e25464caba27c
syzygy333 <brianjonashogue@gmail.com>
2017-05-19 13:45:55 -0400

    First workable version

--- colorcube.js
+++ colorcube.js
@@ -3,33 +3,52 @@
 
 const WHITE  = tinycolor("white");
 const BLACK  = tinycolor("black");
+var colorArray = [];
 
-// To do: get the colors inputted by the user
-
-var color = tinycolor("DA2442");
-document.getElementsByClassName("color")[0].innerHTML = color;
-document.getElementsByClassName("color")[0].style.color = color;
-
-// given color's luminance/readability on white
-var luminance = tinycolor.readability(color, WHITE);
-document.getElementsByClassName("on-white")[0].innerHTML = 'On white = ' + luminance;
-
-// is it readable/is the ratio good?
-var readable = tinycolor.isReadable(color, WHITE);
-if( readable ) {
-  document.getElementsByClassName("on-white")[0].style.color = 'green';
-} else {
-  document.getElementsByClassName("on-white")[0].style.color = 'red';
-}
-
-// given color's luminance/readability on black
-var luminance = tinycolor.readability(color, BLACK);
-document.getElementsByClassName("on-black")[0].innerHTML = 'On black = ' + luminance;
-
-// is it readable/is the ratio good?
-var readable = tinycolor.isReadable(color, BLACK);
-if( readable ) {
-  document.getElementsByClassName("on-black")[0].style.color = 'green';
-} else {
-  document.getElementsByClassName("on-black")[0].style.color = 'red';
+var button = document.querySelector('#brand-color--button');
+button.onclick = function(e) {
+  e.preventDefault();
+  // get the colors inputted by the user
+  var stringInput = document.querySelector('#brand-color--field').value;
+  // turn it into an array
+  colorArray = stringInput.split("\n");
+  // for each value in the array, change the element bg-color
+  for (var i = 0; i < colorArray.length; i++) {
+    // get the first container
+    var div1 = document.getElementsByClassName('color-ratios--column color-ratios--brand-colors')[0];
+    // append a div with the corresponding background color
+    div1.innerHTML = div1.innerHTML + '<div class="color-ratios--swatch" style="background-color: ' + colorArray[i] + '"></div>';
+    
+    // get the color's contrast ratio compared to white
+    var ratio = tinycolor.readability(colorArray[i], WHITE);
+        ratio = Math.round(ratio * 10) / 10;
+    
+    // get the second container
+    var div2 = document.getElementsByClassName('color-ratios--column color-ratios--on-white')[0];
+    
+    // check if the color is readable
+    if ( tinycolor.isReadable(colorArray[i], WHITE) ) {
+      // if it is, append a green div
+      div2.innerHTML = div2.innerHTML + '<div class="color-ratios--swatch pass">' + ratio + '</div>';
+    } else {
+      // append a red div
+      div2.innerHTML = div2.innerHTML + '<div class="color-ratios--swatch fail">' + ratio + '</div>';
+    }
+    
+    // get the color's contrast ratio compared to black
+    var ratio = tinycolor.readability(colorArray[i], BLACK);
+        ratio = Math.round(ratio * 10) / 10;
+    
+    // get the third container
+    var div3 = document.getElementsByClassName('color-ratios--column color-ratios--on-black')[0];
+    
+    // check if the color is readable
+    if ( tinycolor.isReadable(colorArray[i], BLACK) ) {
+      // if it is, append a green div
+      div3.innerHTML = div3.innerHTML + '<div class="color-ratios--swatch pass">' + ratio + '</div>';
+    } else {
+      // append a red div
+      div3.innerHTML = div3.innerHTML + '<div class="color-ratios--swatch fail">' + ratio + '</div>';
+    }
+  }
 }
