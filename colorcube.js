cd5dc6da8ae032a624532d33c9fea063540645fe
Brian Hogue <brianjonashogue@gmail.com>
2019-05-09 12:23:26 -0400

    Adjust wording for clarity

--- colorcube.js
+++ colorcube.js
@@ -88,7 +88,7 @@ function outputRatio(color, ratio, base, target) {
 
   var output =
   `<div class="color-ratio__wrapper ${size} ${passfail}">
-    <div class="color-ratio__label">color over ${base}</div>
+    <div class="color-ratio__label">color with ${base}</div>
     <div class="color-ratio__swatch" style="color: ${base}; border-color: ${base}; background-color: ${color};">Aa</div>
     <span class="fa fa-${iclass}"></span>
     <span class="color-ratio__passfail" title="Color ratio ${ratio} minus target ratio ${target}">
