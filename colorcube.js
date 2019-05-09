902ca6bc2e3d303c25c92e0b754791aed3f1aac1
Brian Hogue <brianjonashogue@gmail.com>
2019-05-09 12:16:24 -0400

    Output color not base
    Fix of the previous commit

--- colorcube.js
+++ colorcube.js
@@ -88,7 +88,7 @@ function outputRatio(color, ratio, base, target) {
 
   var output =
   `<div class="color-ratio__wrapper ${size} ${passfail}">
-    <div class="color-ratio__label">color over ${base}</div>
+    <div class="color-ratio__label">color over ${color}</div>
     <div class="color-ratio__swatch" style="color: ${color}; border-color: ${color}; background-color: ${base};">Aa</div>
     <span class="fa fa-${iclass}"></span>
     <span class="color-ratio__passfail" title="Color ratio ${ratio} minus target ratio ${target}">
