b589f5e9d16e0c94623ce59803db07b2c97c14c4
kimdomenick <kdomenick@thinkoomph.com>
2020-04-30 11:03:59 -0400

    Check local storage, then check OS preferences, only store value if user changes toggle

--- assets/js/toggle-switch.js
+++ assets/js/toggle-switch.js
@@ -1,19 +1,25 @@
 // Collect and store user color preferences but allow the user to change it with a toggle switch
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]'); // Set the toggle switch
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null; // Check for local storage theme
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { // Check OS preferences and set things accordingly
-  toggleSwitch.checked = true;
-  document.documentElement.setAttribute('data-theme', 'dark');
-  localStorage.setItem('theme', 'dark');
+if (currentTheme) { // If local storage theme exists, use that
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
-else {
-  toggleSwitch.checked = false;
-  document.documentElement.setAttribute('data-theme', 'light');
-  localStorage.setItem('theme', 'light');
+else { // If no local storage settings exist
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { // Check OS preferences
+    toggleSwitch.checked = true; // Let's just set the toggle switch.  We don't need to store anything until the user has made a change
+  }
+  else {
+    toggleSwitch.checked = false; // this may not be necessary
+  }
 }
 
-function switchTheme(e) { // Let user switch the theme themselves regardless of settings or past page views
+function switchTheme(e) { // Let user switch the theme themselves regardless of settings or past page views, and store that preference
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
